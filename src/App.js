2ba29b6d8607aafee5db4e17c153f5709a89d192
emadsamy <emadsamy3030@gmail.com>
2023-01-22 01:26:39 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
-import {Routes, Route} from 'react-router-dom';
-import classes from './App.module.css';
-import { Home, Error404, Contact, Menu, ViewProduct } from './views/index';
-import { ScrollToTop } from './components/index';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Routes, Route } from "react-router-dom";
+import classes from "./App.module.css";
+import { Home, Error404, Contact, Menu, ViewProduct } from "./views/index";
+import { ScrollToTop } from "./components/index";
 import { useTranslation } from "react-i18next";
 import i18next from "i18next";
 
 function App() {
-  const [ip, setIP] = useState('');
+  const [ip, setIP] = useState("");
   const { t, i18n } = useTranslation();
 
   const getIp = async () => {
-    const res = await axios.get('https://geolocation-db.com/json/')
+    const res = await axios.get("https://geolocation-db.com/json/");
     // console.log(res.data);
     setIP(res.data);
     visitorHandler(res.data);
-  }
-  
-  useEffect( () => {
+  };
+
+  useEffect(() => {
     getIp();
   }, []);
 
@@ -26,30 +26,29 @@ function App() {
   //   visitorHandler();
   // }, [ip]);
 
-  function visitorHandler (data) {
+  function visitorHandler(data) {
     const options = {
-        url: window.baseURL + "/visitors",
-        method: "POST",
-        data: {
-            ip: data.IPv4,
-            country_code: data.country_code,
-            country_name: data.country_name,
-            lat: data.latitude,
-            lng: data.longitude,
-            language: i18n.language
-        },
+      url: window.baseURL + "/visitors",
+      method: "POST",
+      data: {
+        ip: data.IPv4,
+        country_code: data.country_code,
+        country_name: data.country_name,
+        lat: data.latitude,
+        lng: data.longitude,
+        language: i18n.language,
+      },
     };
 
     axios(options)
-    .then((res) => {
+      .then((res) => {
         // console.log(res);
         // console.log('time');
-    })
-    .catch((err) => {
-      // console.log(err);
-    });
-}
-
+      })
+      .catch((err) => {
+        // console.log(err);
+      });
+  }
 
   return (
     <>
@@ -60,7 +59,7 @@ function App() {
           <Route path="/contact" element={<Contact />} />
           <Route path="/menu" element={<Menu />} />
           <Route path="/view-product/:title/:id" element={<ViewProduct />} />
-          <Route path="*" element={<Error404 />} />
+          <Route path="/error" element={<Error404 />} />
         </Routes>
       </div>
     </>
--- src/i18n.js
+++ src/i18n.js
@@ -3,11 +3,35 @@ import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 
+const resources = {
+  en: {
+    translation: {
+      "brand-title": "Greek Gyros",
+      "home-title": "Home",
+      "menu-title": "Menu",
+      "about-title": "About",
+      "contact-title": "Contact",
+      "delivery-title": "Delivery",
+    },
+  },
+  ru: {
+    translation: {
+      "brand-title": "Греческий гирос",
+      "home-title": "Дом",
+      "menu-title": "Меню",
+      "about-title": "Онас",
+      "contact-title": "Контакт",
+      "delivery-title": "Доставка",
+    },
+  },
+};
+
 i18n
   .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
+    resources,
     lng: localStorage.getItem("i18nextLng") || "ru",
     // fallbackLng: "en",
     debug: true,
